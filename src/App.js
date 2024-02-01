ac9edb6e8a16c006e2f32e688954f890d689a55a
yllipetrovci <yllipetrovci@gmail.com>
2024-02-01 14:52:40 +0100

    Create login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Login from "./pages/Login";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
